3b4bd455d76751a8b74fdc00dafe4ffd6fb61b2f
Andy Meier <andy.meier@visol.ch>
2020-12-02 10:34:23 +0100

    [TASK] auto load TypoScript constants and setup

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,23 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-/***************
+/**
  * PageTS
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page/All.typoscript">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:viresponsiveimages/Configuration/TSconfig/Page/All.typoscript">'
+);
+
+/**
+ * TypoScript Constants
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:viresponsiveimages/Resources/Private/TypoScript/constants.typoscript">'
+);
+
+/**
+ * TypoScript Setup
+ */
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:viresponsiveimages/Resources/Private/TypoScript/setup.typoscript">'
+);
