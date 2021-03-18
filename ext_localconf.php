8f044ee197a75fc7407c0b34228aab0c39bff139
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2021-03-18 23:20:12 +0100

    [TASK] Move TypoScript to Configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,23 +1,28 @@
 <?php
+
 defined('TYPO3') || die();
 
-/**
- * PageTS
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    "@import 'EXT:viresponsiveimages/Configuration/TSconfig/Page/All.typoscript'"
-);
+(function () {
+    /**
+     * PageTS
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        "@import 'EXT:viresponsiveimages/Configuration/TSconfig/Page/All.typoscript'"
+    );
 
-/**
- * TypoScript Constants
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    "@import 'EXT:viresponsiveimages/Resources/Private/TypoScript/constants.typoscript'"
-);
+    /**
+     * TypoScript Constants
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        "@import 'EXT:viresponsiveimages/Configuration/TypoScript/constants.typoscript'"
+    );
 
-/**
- * TypoScript Setup
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    "@import 'EXT:viresponsiveimages/Resources/Private/TypoScript/setup.typoscript'"
-);
+    /**
+     * TypoScript Setup
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        "@import 'EXT:viresponsiveimages/Configuration/TypoScript/setup.typoscript'"
+    );
+})();
+
+
