f2a5f34efc392f14ece9e7ab7e9ead2c4afed9f9
Marcel Lörtscher <marcel.loertscher@visol.ch>
2025-09-10 14:41:51 +0200

    chore(tsconfig): Move base tsconfigs into page.tsconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,29 +1,29 @@
 <?php
-
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-defined('TYPO3') || die();
-
-(function () {
-    /**
-     * PageTS
-     */
-    ExtensionManagementUtility::addPageTSConfig(
-        "@import 'EXT:viresponsiveimages/Configuration/TSconfig/Page/All.typoscript'"
-    );
-
-    /**
-     * TypoScript Constants
-     */
-    ExtensionManagementUtility::addPageTSConfig(
-        "@import 'EXT:viresponsiveimages/Configuration/TypoScript/constants.typoscript'"
-    );
-
-    /**
-     * TypoScript Setup
-     */
-    ExtensionManagementUtility::addPageTSConfig(
-        "@import 'EXT:viresponsiveimages/Configuration/TypoScript/setup.typoscript'"
-    );
-})();
+//
+//use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+//defined('TYPO3') || die();
+//
+//(function () {
+//    /**
+//     * PageTS
+//     */
+//    ExtensionManagementUtility::addPageTSConfig(
+//        "@import 'EXT:viresponsiveimages/Configuration/TSconfig/Page/All.typoscript'"
+//    );
+//
+//    /**
+//     * TypoScript Constants
+//     */
+//    ExtensionManagementUtility::addPageTSConfig(
+//        "@import 'EXT:viresponsiveimages/Configuration/TypoScript/constants.typoscript'"
+//    );
+//
+//    /**
+//     * TypoScript Setup
+//     */
+//    ExtensionManagementUtility::addPageTSConfig(
+//        "@import 'EXT:viresponsiveimages/Configuration/TypoScript/setup.typoscript'"
+//    );
+//})();
 
 
